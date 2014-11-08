05e8735564f7a4bcfee6e5797fd5277ef2e02b73
ngohoaiphuong <ngohoai.phuong@gmail.com>
2014-11-09 00:11:40 +0700

    create my directives

---
+++ my-directives.js
@@ -0,0 +1,17 @@
+angular.module('myDirectives', [])
+  .directive('titleCaseDirective', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      priority: 1,
+      link: function(scope, element, attrs) {
+        if (attrs.type === 'radio' || attrs.type === 'checkbox') return;
+        element.unbind('input').unbind('keydown').unbind('change');
+        element.bind('keydown', function() {
+          scope.$apply(function(){
+            element.val(element.val().replace(/(^([a-z])| [a-z])/g, function(_, match){ return match.toUpperCase();}));
+          })
+        });
+      }
+    }
+  });
