8d81f1e9e92b8417ef4be18db72cd88f4b671f78
ngohoaiphuong <ngohoai.phuong@gmail.com>
2014-11-09 11:46:46 +0700

    trick not working when system keycode

--- my-directives.js
+++ my-directives.js
@@ -6,7 +6,9 @@ angular.module('myDirectives', [])
       priority: 1,
       link: function(scope, element, attrs) {
         if (attrs.type === 'radio' || attrs.type === 'checkbox') return;
-        element.bind('keydown', function() {
+        element.bind('keydown', function(_event_) {
+          var keyCode = _event_.keyCode;
+          if(keyCode === 8 || keyCode == 27 ) return;
           scope.$apply(function(){
             element.val(element.val().replace(/(^(.)| (.))/g, function(_, match){ return match.toUpperCase();}));
           })
