e44e72f395bd85522105b83a0490a9dbf9263329
ngohoaiphuong <ngohoai.phuong@gmail.com>
2014-11-09 11:38:06 +0700

    fix issue: cannot convert first letter to title case

--- my-directives.js
+++ my-directives.js
@@ -8,7 +8,7 @@ angular.module('myDirectives', [])
         if (attrs.type === 'radio' || attrs.type === 'checkbox') return;
         element.bind('keydown', function() {
           scope.$apply(function(){
-            element.val(element.val().replace(/(^([a-z])| [a-z])/g, function(_, match){ return match.toUpperCase();}));
+            element.val(element.val().replace(/(^(.)| (.))/g, function(_, match){ return match.toUpperCase();}));
           })
         });
       }
